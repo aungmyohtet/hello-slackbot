714b32f4845d19f65ff8843dd1f903540405d4f4
aungmyohtet <aungmyohtet@outlook.com>
2017-09-19 16:40:35 +0630

    added Add to slack button

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/hello', function(req, res, next) {
+  res.render('hello', { title: 'Hello slackbot' });
+});
+
+router.get('/register', function(req, res, next) {
+  res.json(req);
+});
+
 module.exports = router;
