f8f4067a31756cb91c0eaf2a2eadc1b548c49324
aungmyohtet <aungmyohtet@outlook.com>
2017-09-19 17:14:23 +0630

    authorization flow added

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+// We need this to build our post string
+var querystring = require('querystring');
+var http = require('http');
+var fs = require('fs');
+
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -11,7 +18,39 @@ router.get('/hello', function(req, res, next) {
 });
 
 router.get('/register', function(req, res, next) {
+  var post_data = querystring.stringify({
+      'client_id' : '57141088304.242744617409',
+      'client_secret': 'fe9100cc60001da2ab018b91417b26bc',
+      'code': req.param('code'),
+      'redirect_uri' : 'http://amh-slackbot.herokuapp.com/register'
+  });
+
+  // An object of options to indicate where to post to
+  var post_options = {
+      host: 'slack.com',
+      port: '80',
+      path: '/api/oauth.access',
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Content-Length': Buffer.byteLength(post_data)
+      }
+  };
+
+  // Set up the request
+  var post_req = http.request(post_options, function(res) {
+      res.setEncoding('utf8');
+      res.on('data', function (chunk) {
+          console.log('Response: ' + chunk);
+      });
+  });
+
+  // post the data
+  post_req.write(post_data);
+  post_req.end();
   res.json(req.param('code'));
 });
+
+
 
 module.exports = router;
