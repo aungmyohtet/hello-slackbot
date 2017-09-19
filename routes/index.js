c03fea578b302f5db89393d85948986591abb45b
aungmyohtet <aungmyohtet@outlook.com>
2017-09-19 17:24:01 +0630

    checking response data of oauth.access

--- routes/index.js
+++ routes/index.js
@@ -42,13 +42,14 @@ router.get('/register', function(req, res, next) {
       res.setEncoding('utf8');
       res.on('data', function (chunk) {
           console.log('Response: ' + chunk);
+          res.json(chunk);
       });
   });
 
   // post the data
   post_req.write(post_data);
   post_req.end();
-  res.json(req.param('code'));
+  //res.json(req.param('code'));
 });
 
 
