25b287ab25b0ae1217093f707f62f0e072cc8d6b
aungmyohtet <aungmyohtet@outlook.com>
2017-09-19 21:40:59 +0630

    test posting

--- routes/index.js
+++ routes/index.js
@@ -41,15 +41,16 @@ router.get('/register', function(req, res, next) {
   var post_req = http.request(post_options, function(res) {
       res.setEncoding('utf8');
       res.on('data', function (chunk) {
-          console.log('Response: ' + chunk);
+          console.log('>>>>>>>>>>>>>>>>>>>>>>>Response: ' + chunk);
       });
   });
 
   // post the data
-  if (req.param('code')) {
+  //if (req.param('code')) {
+    console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>posting data') ;
     post_req.write(post_data);
     post_req.end();
-  }
+  //}
 
   res.json(req.param('code'));
 });
