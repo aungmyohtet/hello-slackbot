7657bdfecb4a2809e14260d80f336787c913013b
aungmyohtet <aungmyohtet@outlook.com>
2017-09-20 08:46:18 +0630

    incoming webhook

--- routes/index.js
+++ routes/index.js
@@ -1,32 +1,45 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var slack = require('slack');
+var slack = require("slack");
+var request = require("request");
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  res.render("index", { title: "Express" });
 });
 
-router.get('/hello', function(req, res, next) {
-  res.render('hello', { title: 'Hello slackbot' });
+router.get("/hello", function(req, res, next) {
+  res.render("hello", { title: "Hello slackbot" });
 });
 
-router.get('/register', function(req, res, next) {
-  if (!req.param('code')) {
-      res.json('No code');
-      return;
+router.get("/register", function(req, res, next) {
+  if (!req.param("code")) {
+    res.json("No code");
+    return;
   }
   var post_data = {
-      'client_id' : '57141088304.242744617409',
-      'client_secret': 'fe9100cc60001da2ab018b91417b26bc',
-      'code': req.param('code'),
-      'redirect_uri' : 'http://amh-slackbot.herokuapp.com/register'
+    client_id: "57141088304.242744617409",
+    client_secret: "fe9100cc60001da2ab018b91417b26bc",
+    code: req.param("code"),
+    redirect_uri: "http://amh-slackbot.herokuapp.com/register"
   };
 
-  slack.oauth.access(post_data, (err, data) => { console.log(">>>>>>> data is " + JSON.stringify(data))});
+  slack.oauth.access(post_data, (err, data) => {
+    console.log(">>>>>>> data is " + JSON.stringify(data));
+    request(
+      {
+        url: data.incoming_webhook.url,
+        method: "POST",
+        json: {
+          text: 'Hello!'
+        }
+      },
+      function(error, response, _data) {
 
-  res.json(req.param('code'));
+      }
+    );
+  });
+
+  res.json(req.param("code"));
 });
-
-
 
 module.exports = router;
